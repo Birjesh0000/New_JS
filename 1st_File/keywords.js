af000cd6908d7e2cba0c1fe6760f5ea9add52abf
Birjesh Gupta <144539291+Birjesh0000@users.noreply.github.com>
2024-06-14 11:31:32 +0000

    it is my second Commit

---
+++ 1st_File/keywords.js
@@ -0,0 +1,12 @@
+const userName = "Birjesh Gupta"
+var password = "hello@Hello"
+let email = "gbirjesh0000@gmail.com"
+const pin = 123456789
+address = "Prayagraj"
+password = "ByeBye"
+email = "gbirjesh123456@gmail.com"
+address = "Kailashnagar"
+
+// userName = "gbirjesh0000"
+console.log(userName);
+console.table([userName, password, email, pin, address])
