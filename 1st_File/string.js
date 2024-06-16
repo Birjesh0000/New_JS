767f2f9799aad258b5c2da353bc8f5c662a5ae13
Birjesh Gupta <144539291+Birjesh0000@users.noreply.github.com>
2024-06-16 13:46:13 +0000

    this repo is for string

---
+++ 1st_File/string.js
@@ -0,0 +1,74 @@
+let name = "Birjesh Gupta"
+let repoCount = 40
+
+console.log(`My name is ${name} and my total repo on github is ${repoCount}`)
+
+let word = new String("Birjesh")
+console.log(word)
+console.log(name[0]);   // to access any particular value at ith (here 0th) key.
+console.log(name.length)
+
+console.log(name.charAt(2));
+console.log(name.indexOf('A'))
+
+let newString = name.substring(0,8);  // return substring from index 0 (included) to 8 (excluded).
+console.log(newString);               // if we enter -ive value, this function ->(substring) reads it 0.
+
+let oneString = name.slice(-8, 8)  //if we enter -ive value, the count starts from reverse side of string.
+console.log(oneString)
+
+//read all properties of string properly
+/*
+anchor: ƒ anchor()
+at: ƒ at()
+big: ƒ big()
+blink: ƒ blink()
+bold: ƒ bold()
+charAt: ƒ charAt()
+charCodeAt: ƒ charCodeAt()
+codePointAt: ƒ codePointAt()
+concat: ƒ concat()
+constructor: ƒ String()
+endsWith: ƒ endsWith()
+fixed: ƒ fixed()
+fontcolor: ƒ fontcolor()
+fontsize: ƒ fontsize()
+includes: ƒ includes()
+indexOf: ƒ indexOf()
+isWellFormed: ƒ isWellFormed()
+italics: ƒ italics()
+lastIndexOf: ƒ lastIndexOf()
+length: 0
+link: ƒ link()
+localeCompare: ƒ localeCompare()
+match: ƒ match()
+matchAll: ƒ matchAll()
+normalize: ƒ normalize()
+padEnd: ƒ padEnd()
+padStart: ƒ padStart()
+repeat: ƒ repeat()
+replace: ƒ replace()
+replaceAll: ƒ replaceAll()
+search: ƒ search()
+slice: ƒ slice()
+small: ƒ small()
+split: ƒ split()
+startsWith: ƒ startsWith()
+strike: ƒ strike()
+sub: ƒ sub()
+substr: ƒ substr()
+substring: ƒ substring()
+sup: ƒ sup()
+toLocaleLowerCase: ƒ toLocaleLowerCase()
+toLocaleUpperCase: ƒ toLocaleUpperCase()
+toLowerCase: ƒ toLowerCase()
+toString: ƒ toString()
+toUpperCase: ƒ toUpperCase()
+toWellFormed: ƒ toWellFormed()
+trim: ƒ trim()
+trimEnd: ƒ trimEnd()
+trimLeft: ƒ trimStart()
+trimRight: ƒ trimEnd()
+trimStart: ƒ trimStart()
+valueOf: ƒ valueOf()
+*/
